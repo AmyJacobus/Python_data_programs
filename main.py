06a33c5c959fbdcc870145d953fe5874b279cc18
Amy <ammishaddaijacobus@gmail.com>
2021-11-02 10:00:52 -0500

    2% - Some docstrings

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+Programmers:
+Date:
+Description:
+"""
+
+# Authorship
+__author__ = 'ADD NAMES HERE'
+__version__ = '1.0'
+__date__ = 'NOV 2, 2021'
+__status__ = 'Development'
+
 
 def main():
     print('Here we run the main program')
---
+++ sports_maintenance.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Programmers:
+Date:
+Description:
+"""
+
+# Authorship
+__author__ = 'ADD NAMES HERE'
+__version__ = '1.0'
+__date__ = 'NOV 2, 2021'
+__status__ = 'Development'
