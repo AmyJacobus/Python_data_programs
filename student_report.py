d20423cadbcc7705cce1fba390a5ea4c91eadff7
Amy <ammishaddaijacobus@gmail.com>
2021-11-08 16:33:08 -0600

    Course module, delete function needs fixing.

--- student_report.py
+++ student_report.py
@@ -1,6 +1,6 @@
 """
-# Programmer: Caleb Fowler
-# Date: Nov 2, 2021
+# Programmer: Caleb Fowler and Ammishaddai Jacobus
+# Date: Nov 10, 2021
 # Description:   STILL NEED DESCRIPTION HERE
 """
 
@@ -14,15 +14,25 @@ __version__ = '1.0'
 __date__ = '2021.11.02'
 _status__ = 'Development'
 
-def menu():
+def display_menu():
+    print('Student Records Report')
+    print('=' * 50)
+    print('1 - List all students report')
+    print('2 - List select student\'s report')
+    print('3 - Delete a student\'s complete report')
+    print('0 - Return to Main Menu')
     print()
 
-def student_data():
-    print()
+
+def selected_student_data():
+    print('Display data for a selected student')
+
+def all_students_data():
+    print('Display data for all students in the database')
 
 
 def main_menu():
-    print('In here we test!')
+    print('In here we test and display everything!')
 
 
 if __name__ == "__main__":  # Basically if the name of the module is equal to main
