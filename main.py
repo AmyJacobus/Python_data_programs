2dd1512faec397aec68d5b080a9270d9b4c402dc
Caleb <cafowl01@wsc.edu>
2021-11-11 09:25:20 -0600

    Added all the docstrings to main

--- main.py
+++ main.py
@@ -22,17 +22,38 @@ _status__ = 'Development'
 
 
 def display_main_menu():
+    """
+    Displays a list of all the valid main menu options
+    It also handles for nonnumerical data and invalid menu option selected
+
+    1-Student Maintenance
+    2-Course Maintenance
+    3-Sport Maintenance
+    4-Full Report
+    0-Exit Program
+
+    :return: n/a
+    """
     print("MAIN MENU")
     print('=================================')
     print("1 - Student Maintenance Menu")
     print("2 - Courses Maintenance Menu")
-    print("3 - Sports Menu")
+    print("3 - Sports Maintenance Menu")
     print("4 - Full Report")
     print("0 - Exit Program")
     print()
 
 
 def main():
+    """
+    Main keep the program looping until the user enters 0 to exit the program
+    then based on the user's selected, will call the corresponding function option
+    Local scoped students is a 2D list that is pass as an argument to each menu option function
+    Local scoped max_student_id is the last student id used, and is passed to the add_student function,
+    and this function will return the last added student id
+
+    :return: n/a
+    """
 
     valid_courses = ('English', 'History', 'Math', 'Science')
     valid_sports = ('Football', 'Volleyball', 'Basketball', 'Track')
